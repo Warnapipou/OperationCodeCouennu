5ebab3377f3b3fa5533feb44e7bfb37a3aee3aac
MlleStrife <majeasi34@gmail.com>
2017-01-22 02:16:28 +0100

    iii

--- ToutNouveauToutBeau/js/index.js
+++ ToutNouveauToutBeau/js/index.js
@@ -1,39 +1,6 @@
 $(document).ready(function(){
     
-    var img_path = 'images/1000bornes';
-    var images = {
-        25: img_path + '25.png',
-        50: img_path + '50.png',
-        75: img_path + '75.png',
-        100: img_path + '100.png',
-        200: img_path + '200.png',
-        vert: img_path + 'vert.png',
-        rouge: img_path + 'rouge.png',
-        accident: img_path + 'accident.png',
-        reparation: img_path + 'reparation.png',
-        panne_essence: img_path + 'panne_essence.png',
-        essence: img_path + 'essence.png',
-        creve: img_path + 'creve.png',
-        roue_secours: img_path + 'roue_secours.png',
-        limite_vitesse: img_path + 'limite_vitesse.png',
-        fin_limite_vitesse: img_path + 'fin_limite_vitesse.png',
-        as_volant: img_path + 'as_volant.png',
-        pompier: img_path + 'pompier.png',
-        citerne: img_path + 'citerne.png',
-        increvable: img_path + 'increvable.png',
-    };
-    
-    var timer = {
-        attentePartie: 1000,
-        monTour: 1000,
-    };
-
-    var idTimer = {
-        debutPartie: undefined,
-        monTour: undefined,
-    };
-    
-    L.Mappy.setImgPath("images/");
+    /*L.Mappy.setImgPath("images/");
     // Création de la carte
     var map = new L.Mappy.Map("map", {
         clientId: 'dri_24hducode',
@@ -48,7 +15,7 @@ $(document).ready(function(){
     // Création d'un marqueur qu'on ajoute au layer
     var marker = L.marker([map._lastCenter.lat, map._lastCenter.lng]).addTo(mLayer);
     var marker = L.marker([48.003767, 0.197248]).addTo(mLayer);
-
+    */
 
 
     /*var sUrl = "https://api.apipagesjaunes.fr/pros/find?what=restaurant&where=43.6044,1.44295&proximity=true&max=5&app_id=d140a6f6&app_key=26452728b034374bccb462e880bfb0e5";
@@ -85,59 +52,93 @@ $(document).ready(function(){
             console.log(jqXHR);
         }
     });
-    
-    // On inscript le joueur.
-    function inscription() {
-        var nom = $('#nom').val();
-        console.log(nom);
-        $.ajax({
-            url: 'serveur',
-            data: {nom : nom},
-            success: function (data, textStatus, jqXHR) {
-                if(data.response == 'ok'){
-                    idTimer.debutPartie = window.setInterval(debutPartie, timer.attentePartie);
-                }
-            }
-        });
-    }
-    
-    // On interroge le serveur afin de savoir quand la partir commence;
-    function debutPartie() {
-        $.ajax({
-            url: 'debutPartie',
-            success: function (data, textStatus, jqXHR) {
-                if(data.response != 'non'){
-                    $("#map").show();
-                    // On affecte les cartes au joueurs.
-                    
-                    $('.carte_main').each(function(){
-                        var img = $(this + 'img');
-                        console.log('image : ' + data.response[img.data('img')]);
-                        img.attr('src', images.response[img.data('img')]);
-                    });
-                    
-                    idTimer.monTour = window.setInterval(monTour, timer.monTour);
-                    window.clearInterval(idTimer.debutPartie);
-                }
-            }
-        });
-    }
 
-    // On demande au serveur si c'est mon tour.
-    function monTour() {
-        $.ajax({
-            url: "/serveur/tour",
-            success: function (data, textStatus, jqXHR) {
-                if(data.response == 'ok'){
-                    $("#loader").hidden();
-                    // On stop les appels ajax, c'est a moi de jouer.
-                    window.clearInterval(idTimer.monTour);
-                }
-                else{
-                    $("#loader").show();
-                }
-            }
-        });
-    }
 });
 
+var img_path = 'images/1000bornes';
+var images = {
+    distance25: img_path + '25.png',
+    distance50: img_path + '50.png',
+    distance75: img_path + '75.png',
+    distance100: img_path + '100.png',
+    distance200: img_path + '200.png',
+    vert: img_path + 'vert.png',
+    rouge: img_path + 'rouge.png',
+    accident: img_path + 'accident.png',
+    reparation: img_path + 'reparation.png',
+    panne_essence: img_path + 'panne_essence.png',
+    essence: img_path + 'essence.png',
+    creve: img_path + 'creve.png',
+    roue_secours: img_path + 'roue_secours.png',
+    limite_vitesse: img_path + 'limite_vitesse.png',
+    fin_limite_vitesse: img_path + 'fin_limite_vitesse.png',
+    as_volant: img_path + 'as_volant.png',
+    pompier: img_path + 'pompier.png',
+    citerne: img_path + 'citerne.png',
+    increvable: img_path + 'increvable.png',
+};
+
+var timer = {
+    attentePartie: 2500,
+    monTour: 1000,
+};
+
+var idTimer = {
+    debutPartie: undefined,
+    monTour: undefined,
+};
+
+// On inscript le joueur.
+function inscription() {
+    var nom = $('#nom').val();
+    $.ajax({
+        url: 'serveur',
+        data: {nom : nom},
+        success: function (data, textStatus, jqXHR) {
+            if(data == 'ok'){
+                idTimer.debutPartie = window.setInterval(debutPartie, timer.attentePartie);
+                console.log("Joueur inscrit.");
+            }
+        }
+    });
+}
+
+// On interroge le serveur afin de savoir quand la partir commence;
+function debutPartie() {
+    $.ajax({
+        url: 'debutPartie',
+        success: function (data, textStatus, jqXHR) {
+            if(data != 'non'){
+                $("#map").show();
+                // On affecte les cartes au joueurs.
+                data = JSON.parse(data);
+                $('.carte_main').each(function(){
+                    var img = $(this).children('img');
+                    img.attr('src', 'images/1000bornes/' + data[img.data('img')] + '.png');
+                });
+
+                //idTimer.monTour = window.setInterval(monTour, timer.monTour);
+                //console.log(idTimer.monTour);
+                window.clearInterval(idTimer.debutPartie);
+            }
+        }
+    });
+}
+
+// On demande au serveur si c'est mon tour.
+function monTour() {
+    $.ajax({
+        url: "/serveur/tour",
+        success: function (data, textStatus, jqXHR) {
+            if(data.response == 'ok'){
+                $("#loader").hidden();
+                // On stop les appels ajax, c'est a moi de jouer.
+                window.clearInterval(idTimer.monTour);
+            }
+            else{
+                $("#loader").show();
+            }
+        }
+    });
+}
+	
